7ebb374e324a377f3cc62b0e5fffb09991e3ec38
shizuka <2358470826@qq.com>
2022-05-11 20:18:37 +0800

    Initial commit

---
+++ src/main/java/com/example/knowledge_graph/KnowledgeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowledge_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/knowledge_graph/KnowledgeGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowledge_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
