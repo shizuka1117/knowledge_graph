1666c25c5a464c288425cd02956a8e687cd673ca
shizuka <2358470826@qq.com>
2022-05-29 10:34:58 +0800

    update the raw data file

--- src/main/java/com/example/knowledge_graph/service/impl/ProblemServiceImpl.java
+++ src/main/java/com/example/knowledge_graph/service/impl/ProblemServiceImpl.java
@@ -1,14 +1,22 @@
 package com.example.knowledge_graph.service.impl;
 
-import com.example.knowledge_graph.entity.Incident;
 import com.example.knowledge_graph.entity.Problem;
+import com.example.knowledge_graph.repository.ProblemRepository;
 import com.example.knowledge_graph.service.ProblemService;
+import org.neo4j.driver.Session;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class ProblemServiceImpl implements ProblemService {
+    @Resource
+    Session session;
+    @Resource
+    ProblemRepository problemRepository;
+
     @Override
     public Problem findProblemById(String id) {
-        return null;
+        return problemRepository.getProblemByNumber(id);
     }
 }
